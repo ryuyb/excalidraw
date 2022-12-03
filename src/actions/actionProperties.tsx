22313ad0aad4c92b92bcb38cb39b67632c505ba0
lion <1203530329@qq.com>
2022-12-03 12:31:18 +0800

    feat: add custom free draw fonts

--- src/actions/actionProperties.tsx
+++ src/actions/actionProperties.tsx
@@ -696,6 +696,21 @@ export const actionChangeFontFamily = register({
         icon: FreedrawIcon,
       },
       {
+        value: FONT_FAMILY.ZiYuQingMei,
+        text: "字语青梅硬笔",
+        icon: FreedrawIcon,
+      },
+      {
+        value: FONT_FAMILY.XiaolaiSC,
+        text: "小赖字体",
+        icon: FreedrawIcon,
+      },
+      {
+        value: FONT_FAMILY.MuYaoRuanBi,
+        text: "沐瑶软笔",
+        icon: FreedrawIcon,
+      },
+      {
         value: FONT_FAMILY.Helvetica,
         text: t("labels.normal"),
         icon: FontFamilyNormalIcon,
@@ -704,7 +719,7 @@ export const actionChangeFontFamily = register({
         value: FONT_FAMILY.Cascadia,
         text: t("labels.code"),
         icon: FontFamilyCodeIcon,
-      },
+      }
     ];
 
     return (
