339bda8b228a7aedefcb7b7bba2433a10d123400
ryuyb <1203530329@qq.com>
2023-01-21 02:34:22 +0000

    feat: delete plus promo icon

--- src/excalidraw-app/index.tsx
+++ src/excalidraw-app/index.tsx
@@ -671,15 +671,6 @@ const ExcalidrawWrapper = () => {
             <WelcomeScreen.Center.MenuItemLiveCollaborationTrigger
               onSelect={() => setCollabDialogShown(true)}
             />
-            {!isExcalidrawPlusSignedUser && (
-              <WelcomeScreen.Center.MenuItemLink
-                href="https://plus.excalidraw.com/plus?utm_source=excalidraw&utm_medium=app&utm_content=welcomeScreenGuest"
-                shortcut={null}
-                icon={PlusPromoIcon}
-              >
-                Try Excalidraw Plus!
-              </WelcomeScreen.Center.MenuItemLink>
-            )}
           </WelcomeScreen.Center.Menu>
         </WelcomeScreen.Center>
       </WelcomeScreen>
