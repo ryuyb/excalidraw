712cb4bf5d7fc7fb4f459dcac234278b6f59f581
lion <1203530329@qq.com>
2023-02-11 20:25:58 +0800

    feat: remove plus in menu

--- src/actions/actionProperties.tsx
+++ src/actions/actionProperties.tsx
@@ -719,7 +719,7 @@ export const actionChangeFontFamily = register({
         value: FONT_FAMILY.Cascadia,
         text: t("labels.code"),
         icon: FontFamilyCodeIcon,
-      }
+      },
     ];
 
     return (
